a37c6d1466a24cb8708d42a0d52f82ed5fe78f31
dsjinwongo <dsgo22jw@naver.com>
2022-01-26 15:19:30 +0900

    set mongod server

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,24 @@
-var express = require("express");
-var bodyparser = require("body-parser");
-var router = require("./router/todolist");
+const express = require("express");
+const routes = require("./routes/todolist");
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
 
-var app = express();
+const app = express();
 
-app.use(router);
-app.use(express.urlencoded({extened: true}))
+mongoose.connect('mongodb://127.0.0.1:27017');
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+    // CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
+});
+
+app.use(routes);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extened: true}));
 
 app.listen(3000, function () {
   console.log("Connected port 3000!")
 })
 
-model.exports=app;
+module.exports=app;
---
+++ backend/routes/todolist.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+//todolist page
+router.get('/', function(res, req){
+    res.render('')
+})
+
+module.exports=router;
