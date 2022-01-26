60bc7aa2cf26145bf15b66f1314d69672adfb104
dsjinwongo <dsgo22jw@naver.com>
2022-01-26 14:06:49 +0900

    reset back-end

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 var express = require("express")
+const { model } = require("mongoose")
 var app = express()
 
-const todoRouter = require('./routes')
+const path = require('path')
 
-app.use('/todolists', todoRouter);
+
+app.use("/node_modules", express.static(path.join(__dirname, "/node_modules")))
+
+app.get("/", function (req, res) {
+  res.send("Hello World!")
+})
 
 
 app.listen(3000, function () {
   console.log("port 3000!")
 })
+
+model.exports=app;
--- backend/router/todolist.js
+++ backend/router/todolist.js
@@ -1,8 +1,1 @@
 const router = require('express').Router();
-const todo = require('../../schemas/todolists');
-const controller = require('./controller');
-const asyncHandler = require('express-async-handler');
-
-const createTodo = asyncHandler(async (req, res, next) => {
-
-});
