ba61628652787b5d2bfb4f24bf782199412dea1c
dsjinwongo <dsgo22jw@naver.com>
2022-01-28 15:15:39 +0900

    add utils

--- backend/routes/controller.js
+++ backend/routes/controller.js
@@ -1,9 +1,10 @@
 const todolist=require('../schemas/todolist');
 const asyncHandler = require('express-async-handler');
+const { createResponse } = require('../utils/response');
 
 const showlist = asyncHandler(async (req, res) => {
     const list= await todolist.find();
-    res.json(list);
+    res.json(createResponse(res, list));
 });
 
 exports.showlist=showlist;
