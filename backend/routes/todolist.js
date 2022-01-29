c003cb19a356214af0e5120dd991d1cd4303135a
dsjinwongo <dsgo22jw@naver.com>
2022-01-29 12:46:38 +0900

    Fix:"fix api router"

--- backend/routes/todolist.js
+++ backend/routes/todolist.js
@@ -2,16 +2,20 @@ const router = require('express').Router();
 const controller=require('./controller');
 
 //메인 페이지
-router.get('/todolist', controller.showlist);
+//router.get('/todolist', controller.showlist);
+router.get('/', controller.showlist)
 
 //할일 추가
-router.post('/addtodo', controller.addtodo);
+//router.post('/addtodo', controller.addtodo);
+router.post('/',controller.addtodo)
 
-//할일 추가
-router.post('/deltodo', controller.deltodo);
+//할일 삭제
+//router.post('/deltodo', controller.deltodo);
+router.delete('/:id', controller.deltodo)
 
 //할일 완료여부수정
-router.post('/updatetodo', controller.updatetodo);
+//router.post('/updatetodo', controller.updatetodo);
+router.put("/:id", controller.updatetodo)
 
 //할일 전부완료
 router.get('/updateall', controller.updateall);
